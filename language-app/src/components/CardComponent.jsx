e955738f792b36fb8ff7e6656737733467506665
EvgeniyaKalinichenko <gusewa.evgenia2011@yandex.ru>
2024-04-08 13:27:06 -0500

    Add focus on button

--- language-app/src/components/CardComponent.jsx
+++ language-app/src/components/CardComponent.jsx
@@ -1,4 +1,5 @@
 import styles from "./Card.module.css";
+import { useRef, useEffect } from "react";
 
 const CardComponent = ({
   english,
@@ -6,7 +7,15 @@ const CardComponent = ({
   russian,
   btnClicked,
   showTranslation,
+  position,
 }) => {
+  const checkButtonRef = useRef(null);
+
+  useEffect(() => {
+    console.log("positions", position);
+    checkButtonRef.current.focus();
+  }, [position]);
+
   return (
     <div className={styles.cardContainer}>
       <div english={english} className="englishWord">
@@ -15,7 +24,11 @@ const CardComponent = ({
       <div className="transcription">{transcription}</div>
       {showTranslation && <div className="russianWord">{russian}</div>}
       {!showTranslation && (
-        <button className={styles.checkButton} onClick={btnClicked}>
+        <button
+          ref={checkButtonRef}
+          className={styles.checkButton}
+          onClick={btnClicked}
+        >
           Проверить
         </button>
       )}
