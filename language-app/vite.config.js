c83bb39593dd998ecb5225adf07199b4a1efacf6
EvgeniyaKalinichenko <gusewa.evgenia2011@yandex.ru>
2025-05-06 17:52:14 -0500

    fix names

--- language-app/vite.config.js
+++ language-app/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/language-app/',
+  base: '/Language-app/',
   plugins: [react()],
   server: {
     port: 3000,
